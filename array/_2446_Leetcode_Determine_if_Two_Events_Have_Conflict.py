50d94618f30582de55085f3320734621a3ae26b7
DinhVanNhan <vannhan10102004@gmail.com>
2025-01-14 14:39:44 +0700

    Leetcode: 2446. Determine if Two Events Have Conflict

---
+++ array/_2446_Leetcode_Determine_if_Two_Events_Have_Conflict.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
+        return event1[1] >= event2[0] and event2[1] >= event1[0]
